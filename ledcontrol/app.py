0c8a1e8ff6870f21aee5b37ce8e893e1127f96ce
tiko09 <timkobelt@gmail.com>
2025-10-02 22:01:12 +0100

    led debug push

--- ledcontrol/app.py
+++ ledcontrol/app.py
@@ -179,6 +179,7 @@ def create_app(led_count,
     led_state = [(0, 0, 0, 0) for _ in range(LED_COUNT)]  # (r,g,b,w)
 
     def push_led_to_hardware(index: int, r: int, g: int, b: int, w: int):
+        app.logger.debug("Push LED to hardware: index=%d r=%d g=%d b=%d w=%d", index, r, g, b, w)
         leds.set_pixel_rgbw(index, r, g, b, w, render=True)
 
     def set_led_rgbw(index: int, r: int, g: int, b: int, w: int):
